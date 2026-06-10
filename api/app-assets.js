be400a1e8676d7aad6b426c13dafd4daa5115dbe
vicky81wu-ai <vicky81wu@gmail.com>
2026-06-10 15:46:51 +0800

    Bump weather patch cache after note rollout

--- api/app-assets.js
+++ api/app-assets.js
@@ -146,7 +146,7 @@ const cloudTextPatchScript = `
 })();
 </script>`;
 
-const weatherPatchScript = '<script src="/assets/weather-patch.js?v=20260610-1"></script>';
+const weatherPatchScript = '<script src="/assets/weather-patch.js?v=20260610-note-cache-1"></script>';
 
 function injectDefaultAssets(html) {
   return String(html)
