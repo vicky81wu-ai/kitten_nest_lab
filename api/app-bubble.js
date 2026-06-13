651a5a957f6f8b9d2196f94c0a1efb96d002c7c2
vicky81wu-ai <vicky81wu@gmail.com>
2026-06-13 17:09:11 +0800

    Bump diagonal CSS sunbeam version

--- api/app-bubble.js
+++ api/app-bubble.js
@@ -1,6 +1,6 @@
 const appAssetctl = require('./app-assetctl');
 
-const coffeeCornerPolishStyle = '<link rel="stylesheet" href="/assets/coffee-corner-polish.css?v=20260613-css-sunbeam-2">';
+const coffeeCornerPolishStyle = '<link rel="stylesheet" href="/assets/coffee-corner-polish.css?v=20260613-css-sunbeam-3">';
 const hotspotPositionerScript = '<script src="/assets/hotspot-positioner.js?v=20260613-bubble-base-1"></script>';
 const coffeeSteamScript = '<script src="/assets/coffee-steam-svg.js?v=20260613-mist-1"></script>';
 const bubbleControllerScript = '<script src="/assets/bubble-controller.js?v=20260610-1"></script>';
