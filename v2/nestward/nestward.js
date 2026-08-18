ad53e4c602a690d4a51ae90d884a56931d2c50a8
vicky81wu-ai <vicky81wu@gmail.com>
2026-08-18 22:41:33 +0800

    feat(nestward): load accepted door transition integration

--- v2/nestward/nestward.js
+++ v2/nestward/nestward.js
@@ -1,4 +1,6 @@
 import { installDoorAwayIntegrationV2 } from './door-away-integration-v2.js';
+import { installDoorTransitionIntegration } from './door-transition-integration.js';
 
 installDoorAwayIntegrationV2();
+installDoorTransitionIntegration();
 await import('./nestward-core.js');
